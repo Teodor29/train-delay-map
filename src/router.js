9cd3254f81558ac09c91a0e08b42f6e0201d51df
Teodor29 <teodor.lien@gmail.com>
2025-05-21 22:33:06 +0200

    First commit

---
+++ src/router.js
@@ -0,0 +1,53 @@
+export default class Router extends HTMLElement {
+    constructor() {
+        super();
+
+        this.currentRoute = "";
+
+        this.allRoutes = {
+            "": {
+                view: "<map-view></map-view>",
+                name: "Map",
+            },
+            "saved": {
+                view: "<saved-view></saved-view>",
+                name: "Saved",
+            },
+        };
+    }
+
+    get routes() {
+        return this.allRoutes;
+    }
+
+    // connect component
+    connectedCallback() {
+        window.addEventListener("hashchange", () => {
+            this.resolveRoute();
+        });
+
+        this.resolveRoute();
+    }
+
+    resolveRoute() {
+        this.currentRoute = location.hash.replace("#", "").split("/")[0];
+
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `
+            <div class="route-container">
+                <not-found></not-found>
+            </div>
+        `;
+
+        if (this.routes[this.currentRoute]) {
+            this.innerHTML = `
+                <div class="route-container">
+                    ${this.routes[this.currentRoute].view}
+                </div>
+            `;
+        }
+    }
+}
