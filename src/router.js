7358f2edee980c3681a38c74d454774f7345f1d3
Teodor29 <teodor.lien@gmail.com>
2026-01-11 14:29:05 +0100

    Remove console.log and Format code with Prettier

--- src/router.js
+++ src/router.js
@@ -1,64 +1,63 @@
 export default class Router extends HTMLElement {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.currentRoute = "";
+    this.currentRoute = "";
 
-        this.allRoutes = {
-            "": {
-                view: "<map-view></map-view>",
-                name: "Karta",
-                icon: "map",
-            },
-            saved: {
-                view: "<saved-view></saved-view>",
-                name: "Sparade",
-                icon: "saved",
-            },
-            login: {
-                view: "<login-form></login-form>",
-                name: "Logga in",
-            },
-            register: {
-                view: "<register-form></register-form>",
-                name: "Registrera",
-            },
-        };
-    }
+    this.allRoutes = {
+      "": {
+        view: "<map-view></map-view>",
+        name: "Karta",
+        icon: "map",
+      },
+      saved: {
+        view: "<saved-view></saved-view>",
+        name: "Sparade",
+        icon: "saved",
+      },
+      login: {
+        view: "<login-form></login-form>",
+        name: "Logga in",
+      },
+      register: {
+        view: "<register-form></register-form>",
+        name: "Registrera",
+      },
+    };
+  }
 
-    get routes() {
-        return this.allRoutes;
-    }
+  get routes() {
+    return this.allRoutes;
+  }
 
-    // connect component
-    connectedCallback() {
-        window.addEventListener("hashchange", () => {
-            this.resolveRoute();
-        });
+  // connect component
+  connectedCallback() {
+    window.addEventListener("hashchange", () => {
+      this.resolveRoute();
+    });
 
-        this.resolveRoute();
-    }
+    this.resolveRoute();
+  }
 
-    resolveRoute() {
-        this.currentRoute = location.hash.replace("#", "").split("/")[0];
+  resolveRoute() {
+    this.currentRoute = location.hash.replace("#", "").split("/")[0];
 
-        this.render();
-    }
+    this.render();
+  }
 
-    render() {
-        this.innerHTML = `
+  render() {
+    this.innerHTML = `
             <div class="route-container">
                 <not-found></not-found>
             </div>
         `;
 
-        if (this.routes[this.currentRoute]) {
-            this.innerHTML = `
+    if (this.routes[this.currentRoute]) {
+      this.innerHTML = `
                 <div class="route-container">
                     ${this.routes[this.currentRoute].view}
                 </div>
             `;
-        }
-
     }
+  }
 }
